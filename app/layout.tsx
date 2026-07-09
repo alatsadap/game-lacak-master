9a068f6c34fb3de361d1bf215cfb80d151ced49f
mbah <149068396+alatsadap@users.noreply.github.com>
2026-07-09 21:41:58 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: 'Game Lacak',
+    default: 'Game Tes Kecerdasan',
     template: '%s'
   },
   description: 'Aplikasi Tracking Lokasi dengan Dashboard Admin',
